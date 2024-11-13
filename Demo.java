a66903af37a0b8053d8a7a4454d4668a5bc79458
anil <h.anilkumar5478@gmail.com>
2024-11-13 11:17:04 +0530

    multiplication

--- Demo.java
+++ Demo.java
@@ -1,7 +1,10 @@
 public class Demo{
   public static void main(String args[]){
     System.out.print("hello");
-System.out.print("Anil");
+    System.out.print("Anil");
+    int x=20;
+    int y=30;
+    int z=x*y;
    
   }
 }
